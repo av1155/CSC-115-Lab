0badb02fd5fe85299f8d8fff016646185c861fd5
av1155 <andrea.venti12@gmail.com>
2023-01-18 11:52:31 -0500

    Update 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ wage = 20
 hours = 8
 weeks = 52
 salary = wage * hours * weeks
-print('Salary is:',salary)
+print('Salary is:', salary)
 
 hours = 4
 weeks = 16
@@ -11,3 +11,5 @@ salary = 15
 salary = wage * hours * weeks
 
 print('New Salary is:', salary)
+
+# With a hashtag you can make comments. A cool thing about Python is that it is very easy to understand for beginners. I have linked all of this to my GitHub repository and I am able to save my work on the cloud this way. Wage, hours, and weeks are all expressions because they yield a value. In this case, they yield the values, 20, 8, and 52. Then you can use the expression "salary" to get a result. In this new expression you can write down that wage * hours * weeks = the new salary. Print is used to print text and then show the result. In this case, I "print" the new salary.
