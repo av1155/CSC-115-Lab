15f0ced398529c3a19c06833d2cdfbbc0596ef3a
av1155 <andrea.venti12@gmail.com>
2023-01-18 11:39:41 -0500

    CSC 115 Lab

---
+++ main.py
@@ -0,0 +1,13 @@
+wage = 20
+hours = 8
+weeks = 52
+salary = wage * hours * weeks
+print('Salary is:',salary)
+
+hours = 4
+weeks = 16
+salary = 15
+
+salary = wage * hours * weeks
+
+print('New Salary is:', salary)
