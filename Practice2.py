0af4fa44a2cc1d7885a960e47364c28ad22ebc4a
av1155 <andrea.venti12@gmail.com>
2023-01-19 23:53:36 -0500

    practice2_first

---
+++ Practice2.py
@@ -0,0 +1,9 @@
+print('Current salary is:')
+current_salary = int(input())
+print('Current salary is:', current_salary)
+
+print()
+
+print('New salary is:')
+new_salary = int(input())
+print('New salary is:', new_salary)
