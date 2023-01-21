fbe91600b6c77e054c2418bc9d494bd8f31a3367
av1155 <andrea.venti12@gmail.com>
2023-01-21 14:00:17 -0500

    final_update

--- Chapter 2/practice3.py
+++ Chapter 2/practice3.py
@@ -1,3 +1,7 @@
+import math
+from pickletools import pyfloat
+
+
 print('Variables and assignments')
 
 print('3 2 1 Go!')
@@ -41,3 +45,60 @@ print('Your total amount of cents is:', total_cents)
 dollar_amount = total_cents / 100
 
 print('Your total amount of dollars is:', dollar_amount)
+
+'''
+Python 3 reserved keywords
+False      await      else       import     pass
+None       break      except     in         raise
+True       class      finally    is         return
+and        continue   for        lambda     try
+as         def        from       nonlocal   while
+assert     del        global     not        with
+async      elif       if         or         yield
+'''
+
+'''
+2.4 Numeric types: Floating-point
+
+There are floating-point numbers and floating-point literal.
+A floating-point number is any number that has a decimal point appearing anywhere in the number. For example 98.6, or 0.00001, or -88.653.
+A floating-point literal is written with the fractional part even if that fraction is 0, as in 1.0, 0.0, or 99.0.
+
+Float is a data type for floating-point numbers.
+'''
+
+miles = float(input('Enter a distance in miles: '))
+
+hours_to_fly = miles / 500
+hours_to_drive = miles / 60
+
+print(miles, 'miles would take')
+print(hours_to_fly, 'hours to fly')
+print(hours_to_drive, 'hours to drive')
+
+
+print('2.0 to the power of 512 = ', 2.0 ** 512)
+
+temperature_canada_fahrenheit = float(input('Temperature in Canada: '))
+temperature_panama_fahrenheit = float(input('Temperature in Panama: '))
+
+average_fahrenheit = (temperature_canada_fahrenheit +
+                      temperature_panama_fahrenheit) / 2
+
+print(f'{average_fahrenheit:.2f}', 'is the average fahrenheit temperature.')
+
+
+print('Default output of Pi:', math.pi)
+print('Pi reduced to 4 digits after the decimal:', end=' ')
+print(f'{math.pi:.4f}')
+
+
+# Below is the Challenge Activity of the section.
+
+wall_area = float(input('How many square feet do you want to paint? '))
+
+# Assign gallons_paint below
+
+gallons_paint = wall_area / 350
+
+print(f'{gallons_paint:.3}')
