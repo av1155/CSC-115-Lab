5378fa9aa98cc8e4090f0baf2a4389d289b89945
av1155 <andrea.venti12@gmail.com>
2023-01-21 12:53:29 -0500

    update2

--- Chapter 2/practice3.py
+++ Chapter 2/practice3.py
@@ -25,3 +25,19 @@ x = people_with_measles * 20 * 20
 
 print(people_with_measles, 'people with measles may cause',
       x, 'people to be infected per week')
+
+
+nickel_count = int(input('How many nickels do you have? '))
+dime_count = int(input('How many dimes do you have? '))
+
+total_nickel = nickel_count * 5
+
+total_dime = dime_count * 10
+
+total_cents = total_nickel + total_dime
+
+print('Your total amount of cents is:', total_cents)
+
+dollar_amount = total_cents / 100
+
+print('Your total amount of dollars is:', dollar_amount)
