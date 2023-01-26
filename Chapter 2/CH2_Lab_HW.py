bf3efa2a868a2ff0e85c1d0ec3afe8e0b4265e62
av1155 <andrea.venti12@gmail.com>
2023-01-25 21:59:58 -0500

    still_working_HW

--- Chapter 2/CH2_Lab_HW.py
+++ Chapter 2/CH2_Lab_HW.py
@@ -11,13 +11,12 @@ result_3 = result_2 // div_num
 
 print(result, result_2, result_3)
 
-''' 2.13 Lab: Driving costs '''
+''' 2.14 Lab: Driving costs '''
 
 print()
 
 
-''' OUTPUT the gas cost for 25, 75, and 500 miles.
-    INPUT gas_mileage, and gas_cost '''
+# OUTPUT the gas cost for 25, 75, and 500 miles. INPUT gas_mileage, and gas_cost.
 
 distance = 20
 distance_2 = 75
@@ -30,3 +29,15 @@ result2 = (distance_2 / gas_mileage) * gas_cost
 result3 = (distance_3 / gas_mileage) * gas_cost
 
 print(f'{result1:.2f} {result2:.2f} {result3:.2f}')
+
+''' 2.15 Lab: Expression for calories burned druing workout '''
+
+age = int(input())
+weight = int(input())
+heart_rate = int(input())
+time = int(input())
+
+calories_burned = (((age * 0.2757) + (weight * 0.03295) +
+                   (heart_rate * 1.0781) - 75.4991) * time) / 8.368
+
+print('Calories:', f'{calories_burned:.2}', 'calories')
