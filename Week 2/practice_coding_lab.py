7b018960a01f8ea274d6e25d4e418b3262ee254f
av1155 <andrea.venti12@gmail.com>
2023-01-27 10:42:39 -0500

    update

---
+++ Week 2/practice_coding_lab.py
@@ -0,0 +1,23 @@
+print('6/4')
+
+print(6 / 4)
+
+print(6 // 4)
+
+# Floor division is the integer of the division.
+
+print(6 % 4)
+print(50 % 2)
+
+# Remainder of the division.
+
+'''----------------------------------------------------------------'''
+
+first_number = float(input('Enter a number: '))
+second_number = float(input('Enter a number: '))
+
+sum = first_number + second_number
+
+print(sum)
+
+print(sum ** 10)
