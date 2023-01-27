6c46649d5b57ce8ed682caaff5ae11c074185d62
av1155 <andrea.venti12@gmail.com>
2023-01-27 11:09:54 -0500

    update2

---
+++ Week 2/import_random.py
@@ -0,0 +1,9 @@
+import random
+
+print(random.random())
+
+print(random.randrange(10))
+# Prints a random number between 0 and 9.
+
+print(random.randint(1, 10))
+# Print random numbers between 1 and 10.
--- Week 2/practice_coding_lab.py
+++ Week 2/practice_coding_lab.py
@@ -18,6 +18,13 @@ second_number = float(input('Enter a number: '))
 
 sum = first_number + second_number
 
-print(sum)
+print('The sum of', first_number, 'and', second_number, 'is:', sum)
 
-print(sum ** 10)
+print()
+
+print(
+    f'The result of the sum of{first_number: .2f} and{second_number: .2f} to the power of 10 is:{sum ** 10: .2f} ')
+
+# This is to output the entirety of the string with 2 decimals. There is only two ' ', one at the start AFTER f, and one at the end.
+
+'''---------------------------------------------------------------------------------------------------------------------------------------'''
