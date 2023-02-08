597673a92619815d232ded27c5a3412242470b7e
av1155 <andrea.venti12@gmail.com>
2023-02-08 09:56:32 -0500

    uodate

--- Week 4/4.1_If-else_Branches_(General).py
+++ Week 4/4.1_If-else_Branches_(General).py
@@ -10,3 +10,24 @@ if user_age > 65:
     hotel_rate = hotel_rate - 30
 
 print("Your rate: ", hotel_rate)
+
+print()
+
+val = int(input("Enter positive or negative value: "))
+
+if val < 0:
+    val = val
+
+print(-val)
+
+print()
+
+user_age = int(input("Enter your age: "))
+if user_age < 25:
+    insure_price = 4800
+else:
+    insure_price = 2200
+
+print(f"Annual price: ${insure_price}")
+
+print()
--- Week 4/lecture.py
+++ Week 4/lecture.py
@@ -1,3 +1,5 @@
+# CORRECT SYNTAX
+
 name = "Andrea Venti Fuentes"
 
 gpa = 4.0
@@ -5,3 +7,52 @@ gpa = 4.0
 major = "Computer Science"
 
 age = 22
+
+print(name + ", " + str(gpa) + ", " + major + ", " + str(age))
+
+print(f"{name}, {str(gpa)}, {major}, {str(age)}")
+
+print()
+
+# CORRECT SYNTAX
+
+# Arithmetic
+# +
+# -
+# *
+# /
+# %
+
+# Relational Operators
+# < >
+# <= >=
+# ==
+# != <>
+
+age = int(input("How old are you? "))
+
+if (age >= 21):
+    print("You can drink.")
+else:  # Else is not followed by a conditon, it uses the if coniditon.
+    print("You cannot drink.")
+
+print("Program Complete.")
+
+print()
+
+your_score = float(input("What is your score? "))
+
+if (your_score >= 90):
+    print("You got an A")
+
+elif (your_score >= 80):
+    print("You got a B")
+
+elif (your_score >= 70):
+    print("You got a C")
+
+elif (your_score >= 60):
+    print("You got a D")
+
+else:
+    print("You got an F")
