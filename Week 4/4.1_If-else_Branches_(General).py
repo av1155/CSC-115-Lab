5ec7116c0257b67ad155d0e5ff7f8700e7217ddd
av1155 <andrea.venti12@gmail.com>
2023-02-08 09:08:36 -0500

    new_file

---
+++ Week 4/4.1_If-else_Branches_(General).py
@@ -0,0 +1,4 @@
+print("4.1: If-else branches (General)")
+
+# Branch: Sequence of statements only executed under a certain condition.
+# If: An if branch is a branch taken only if an expression is true.
