714c59f7040a5aa597ba1977b96c2704d415d684
av1155 <andrea.venti12@gmail.com>
2023-02-09 15:01:50 -0500

    update

--- Week 4/4.1_If-else_Branches_(General).py
+++ Week 4/4.1_If-else_Branches_(General).py
@@ -13,6 +13,9 @@ print("Your rate: ", hotel_rate)
 
 print()
 
+# If-else Branches
+# If-else branch: It has two branches: The first branch is executed if an expression is true, else the other branch is executed.
+
 val = int(input("Enter positive or negative value: "))
 
 if val < 0:
@@ -31,3 +34,10 @@ else:
 print(f"Annual price: ${insure_price}")
 
 print()
+
+# If-elseif-else Branches
+# Commonly a programmer wishes to take one of multiple (three or more) branches. An if-else can be extended to an if-elseif-else structure. Each branch's expression is checked in sequence; as soon as one branch's expression is found to be true, that branch is taken. If no expression is found true, execution will reach the else branch, which then executes.
+
+# Note: The else part is optional. If omitted, then if none of the previous expressions are true, no branch executes.
+
+# If the same branch is repeated later, that branch will never execute because the one that is before will execute first.
