76c9c4c52104dad01a224969d17c40e279de4f32
av1155 <andrea.venti12@gmail.com>
2023-02-10 12:13:23 -0500

    update

--- Week 4/lab_friday.py
+++ Week 4/lab_friday.py
@@ -49,7 +49,7 @@ else:
 if (testScore == 100):
     print("Congratulations! You earned a perfect test score")
 '''
-
+'''
 # Given 3 numbers: X, Y, and Z. Find minimum.
 x = int(input())
 y = int(input())
@@ -68,3 +68,61 @@ elif (y < x) and (y < z):
 else:
     min = z
     print(min)
+'''
+'''
+x = int(input())
+y = int(input())
+z = int(input())
+
+min = x
+if (y < min):
+    min = y
+if (z < min):
+    min = z
+
+print(min)
+'''
+
+totalChange = int(input())
+if (totalChange == 0):
+    print("No change")
+
+dollar = totalChange // 100
+totalChange = totalChange % 100
+
+quarter = totalChange // 25
+totalChange = totalChange % 25
+
+dime = totalChange // 10
+totalChange = totalChange % 10
+
+nickel = totalChange // 5
+totalChange = total = totalChange % 5
+
+penny = totalChange // 1
+
+
+if (dollar == 1):
+    print(dollar, "Dollar")
+elif (dollar > 1):
+    print(dollar, "Dollars")
+
+if (quarter == 1):
+    print(quarter, "Quarter")
+elif (quarter > 1):
+    print(quarter, "Quarters")
+
+if (dime == 1):
+    print(dime, "Dime")
+elif (dime > 1):
+    print(dime, "Dimes")
+
+if (nickel == 1):
+    print(nickel, "Nickel")
+elif (nickel > 1):
+    print(nickel, "Nickels")
+
+if (penny == 1):
+    print(penny, "Penny")
+elif (penny > 1):
+    print(penny, "Pennies")
