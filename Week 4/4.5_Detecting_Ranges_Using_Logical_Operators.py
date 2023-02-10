4830e8839ce558b1bbf419fffb88a12f3b429f7b
av1155 <andrea.venti12@gmail.com>
2023-02-09 23:56:59 -0500

    yay

--- Week 4/4.5_Detecting_Ranges_Using_Logical_Operators.py
+++ Week 4/4.5_Detecting_Ranges_Using_Logical_Operators.py
@@ -27,3 +27,23 @@ else:
 
 # Boolean and logical operators
 # Boolean: refers to a value that is either True or False. (True and False must be capitalized). A programmer can assign a Boolean value by specifying True or False, or by evaluating an expression that yields a Boolean.
+
+# Boolean AND: True when both operands are True.
+# Boolean OR: True when at least one operand is True.
+# Boolean NOT (opposite): True when the single operand is False (and False when operand is True).
+
+user_channel = int(input("Enter Cable TV Channel Number: "))
+
+if (user_channel >= 2) and (user_channel <= 499):
+    channel_type = 'Standard Definition'
+
+elif (user_channel >= 500) and (user_channel <= 599):
+    channel_type = 'Paid Channel'
+
+elif (user_channel >= 1002) and (user_channel <= 1499):
+    channel_type = 'High Definition'
+
+else:
+    channel_type = 'Error'
+
+print(f"Channel type: {channel_type}")
