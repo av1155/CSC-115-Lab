232826676074f9c269b17fa4209731d041b1c117
av1155 <andrea.venti12@gmail.com>
2023-02-17 11:33:33 -0500

    a

--- Week 6/5.1_Loops_General.py
+++ Week 6/5.1_Loops_General.py
@@ -1,25 +1,17 @@
-# This prints the MINIMUM value
-x = float(input())
-y = float(input())
-z = float(input())
+# Loop: program construct that repeatedly executes the loop's statements (known as the loop body) while the loop's expression is true; when the expression is false, execution proceeds past the loop. Each time through a loop's statements is called an iteration.
 
-min = x
-if (y < min):
-    min = y
-if (z < min):
-    min = z
+num_list = [2, 4, 9, -1]
 
-print(f"The minimum value is {min}")
+sum1 = 0
+number = 0
 
-# This prints the MAXIMUM value
-a = float(input())
-b = float(input())
-c = float(input())
+i = 0
+while (i < len(num_list)):
+    sum1 = sum1 + num_list[i]
+    i = i+1
 
-max = a
-if (b > max):
-    max = b
-if (c > max):
-    max = c
+avg = sum1 / len(num_list)
+print("The sum is ", sum1)
+print(f"The average is {avg}")
 
-print(f"The maximum value is {max}")
+print()
