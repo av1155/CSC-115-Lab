d1d3a97689c113b4055596e311be6165bf323112
av1155 <andrea.venti12@gmail.com>
2023-02-24 16:20:42 -0500

    finished_lab

--- Week 6/CH.5_Lab_Homework.py
+++ Week 6/CH.5_Lab_Homework.py
@@ -8,12 +8,9 @@ count = len(user_text)
 for char in list(user_text):
     if char in special_character_list:
         count -= 1
-print(count)
+print(count, "\n")
 
-# 8 minutes
-
-print()
-
+##########################################################################
 # 5.16 LAB: Password modifier
 word = input("Enter a password: ")
 password = ""
@@ -36,6 +33,7 @@ for character in word:
 password += "!"
 print(password, "\n")
 
+##########################################################################
 # 5.17 LAB: Output range with increment of 5
 first_num = int(input("Please enter your first number: "))
 second_num = int(input("Please enter your second number: "))
@@ -47,9 +45,36 @@ else:
         print(i, end=" ")
 print()
 
+##########################################################################
+# 5.19 LAB: Countdown until matching digits
+number = int(input("\nPlease enter a number: "))
+while (number < 11) or (number > 100):
+    number = int(input("\nInput must be 11-100: "))
+
+while (number > 11) and (number <= 100):
+    print(number)
+    number -= 1
+    if (number % 11) == 0:
+        print(number)
+        break
+
+# number = int(input())
+# if (number < 11) or (number > 100):
+#     print("Input must be 11-100")
+
+# else:
+#     while (number >= 11) and (number <= 100):
+#         print(number)
+#         number -= 1
+#         if (number % 11) == 0:
+#             print(number)
+#             break
+
+##########################################################################
 # 5.18 LAB: Print string in reverse
 while True:
-    text = str(input("\nPlease enter text (enter d, or done, or Done to quit): "))
+    text = str(
+        input("\nPlease enter text (enter d, or done, or Done to quit): \n"))
 
     if text == "Done":
         quit()
