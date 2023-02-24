96be6cca8113a3849af9527e926c92033f1f714e
av1155 <andrea.venti12@gmail.com>
2023-02-24 18:44:41 -0500

    improved code

--- VentiFuentes_Andrea_Midterm_Project_Assignment.py
+++ VentiFuentes_Andrea_Midterm_Project_Assignment.py
@@ -8,7 +8,8 @@ starting_num_organisms = float(
     input("Enter the starting number of organisms: "))
 while starting_num_organisms < 0:
     starting_num_organisms = float(input(
-        "A negative number for the starting number of organisms is not allowed. Please re-enter your starting number of organisms value: "))
+        "Negative values for the starting number of organisms are not allowed. Please re-enter the starting number of organisms: "))
+print()
 
 # The input below records the average daily population increase.
 avg_daily_population_increase = float(
@@ -16,14 +17,16 @@ avg_daily_population_increase = float(
 # The while loop below validates the input and requests the user to input a new value if the numbers are either negative or higher than
 while (avg_daily_population_increase < 0) or (avg_daily_population_increase > 100):
     avg_daily_population_increase = float(input(
-        "Negative values or values higher than 100 are not allowed. Please re-enter your average daily population increase: "))
+        "Negative values or values larger than 100 are not allowed. Please re-enter the average daily population increase: "))
+print()
 
 # The input below records the number of days that the organisms will be left to multiply for.
 days_to_multiply = float(
     input("Enter the number of days the organism will multiply for: "))
-while (days_to_multiply > 30):
+while (days_to_multiply > 30) or (days_to_multiply < 0):
     days_to_multiply = float(input(
-        "No more than 30 days is allowed for the number of days to multiply. Please re-enter your days to multiply value: "))
+        "Negative values or values larger than 30 days are not allowed. Please re-enter the number of days the organism will be left to multiply for: "))
+print()
 
 # There have to be three inputs.
 # Validate program. Number of days has to be between 0 and 100. No negative numbers and no numbers avobe 100. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
