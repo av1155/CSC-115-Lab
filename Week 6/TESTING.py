866df28dad92df9593d21897be65be795bf575e9
av1155 <andrea.venti12@gmail.com>
2023-02-25 11:43:03 -0500

    a

--- Week 6/TESTING.py
+++
@@ -1,9 +0,0 @@
-population = int(input("Enter the starting number of organisms: "))
-DailyIncrease = float(input(
-    "Enter the average daily population increase (number will be translated to percent value, for example, 30 means 30%): "))
-DailyIncrease = ((DailyIncrease + 100) / 100)
-Days = int(input("Enter the number of days the organism will multiply for: "))
-print("Day Approximate\t Population")
-for i in range(1, Days + 1):
-    print(i, "\t\t", population)
-    population = population * DailyIncrease
