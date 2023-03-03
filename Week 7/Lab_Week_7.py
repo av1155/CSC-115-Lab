12782146285a9d02f8ee86e70d797c734ab87427
av1155 <andrea.venti12@gmail.com>
2023-03-03 10:47:05 -0500

    a

--- Week 7/Lab_Week_7.py
+++ Week 7/Lab_Week_7.py
@@ -3,3 +3,27 @@ user_input = input("Enter something: ")
 while (user_input != "done") and (user_input != "Done"):
     print(user_input)
     user_input = input("Enter something...")
+
+
+stop = int(input())
+result = 0
+for a in range(4):
+    for b in range(3):
+        result += a + b
+    print(result)
+    if result > stop:
+        break
+
+"""
+            RAM
+Stop:       
+Result:
+a:
+b:
+
+Output: 
+3
+9
+18
+
+"""
